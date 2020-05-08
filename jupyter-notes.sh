baa8560bc3d4c3e7191eb8a19d97a5922abaa0cc
Dan Rozelle <dan.rozelle@ranchobiosciences.com>
2020-05-08 12:19:03 -0400

    rename

---
+++ jupyter-notes.sh
@@ -0,0 +1,22 @@
+# setup a conda env with jupyter 
+conda create -n bids python=3.7 ipykernel jupyter
+source activate bids
+conda install -c conda-forge pandas
+python -m ipykernel install --user --name=bids
+jupyter notebook
+
+# installing packages from inside jupyter
+import sys
+!conda install --yes --prefix {sys.prefix} pandas
+
+# import src modules and forced re-import
+import importlib
+from src.util import *
+importlib.reload(src.util)
+
+# connect to jupyter server and run locally
+# on cygwin, forward post 8889 from server 43
+ssh -L localhost:8889:localhost:8889 user@10.0.0.0
+
+# launch the notebook
+jupyter notebook --no-browser --port=8889
