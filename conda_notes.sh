3048a7f325cd2450df01b32378be863571ee4fbf
Dan Rozelle <dkrozelle@users.noreply.github.com>
2022-06-21 15:36:13 -0400

    Update conda_notes.sh

--- conda_notes.sh
+++ conda_notes.sh
@@ -1,6 +1,20 @@
-### setup a conda env with jupyter 
+# setup a conda env with jupyter 
 conda create -n myenv python=3.7 pandas 
 conda activate myenv
 conda install -c conda-forge pandas
 
 conda env remove -n myenv
+
+conda env export >environment.yaml
+conda env create -f environment.yaml
+
+# using conda in dockerfile
+USER ubuntu
+RUN conda create -n myenv python=3.9
+SHELL ["conda", "run", "-n", "myenv", "/bin/bash", "-c"]
+RUN conda install -c conda-forge -c anaconda pigz pyyaml
+
+RUN conda init bash
+
+# running a script from a conda env
+conda run -n myenv script.sh
