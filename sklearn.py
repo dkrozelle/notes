673c1fa93acd27f97f4ba4ff772fdc42855b374f
Dan Rozelle <dkrozelle@users.noreply.github.com>
2023-06-17 13:52:27 -0400

    Cross validation

--- sklearn.py
+++ sklearn.py
@@ -1,4 +1,4 @@
-# Import LinearRegression
+# LinearRegression
 from sklearn.linear_model import LinearRegression
 
 # Create the model
@@ -14,3 +14,26 @@ reg.fit(X,y)
 predictions = reg.predict(X)
 
 print(predictions[:5])
+
+
+# CrossValidation
+from sklearn.model_selection import cross_val_score, KFold
+
+# Create a KFold object
+kf = KFold(n_splits=6, shuffle=True, random_state=5)
+
+reg = LinearRegression()
+
+# Compute 6-fold cross-validation scores
+cv_scores = cross_val_score(reg, X, y, cv=kf)
+
+# Print scores
+print(cv_scores)
+# Print the mean
+print(np.mean(cv_results))
+
+# Print the standard deviation
+print(np.std(cv_results))
+
+# Print the 95% confidence interval
+print(np.quantile(cv_results, [.025, .975]))
