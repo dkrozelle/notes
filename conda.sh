51d27dc4341f990b43e8bc694aadd5fdd30b7c6e
Dan Rozelle <dan.rozelle@ranchobiosciences.com>
2024-04-09 21:02:20 -0400

    add mamba info

--- conda.sh
+++ conda.sh
@@ -1,17 +1,33 @@
+# install mamba miniforge
+curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
+bash Miniforge3-$(uname)-$(uname -m).sh
+
+
 # setup a conda env with jupyter 
 conda create -n ENV_NAME python=3.7 pandas 
+or
+conda env create -f environment.yaml
 conda activate ENV_NAME
 conda install -c conda-forge pandas
 
-# uninstall conda
-rm -r ~/miniconda/
+# use mamba resolver and add channels
+conda install conda-libmamba-solver
+conda config --set solver libmamba
+
+conda config --add channels bioconda 
+conda config --add channels conda-forge 
+conda config --add channels biocore
+conda config --show channels
 
 # work with an environment yaml file
 conda env export >environment.yaml
-conda env create -f environment.yaml
+
 
 # remove a named environment
 conda remove -n ENV_NAME --all
+
+# uninstall conda
+rm -r ~/miniconda/
 
 # using conda in dockerfile
 USER ubuntu
@@ -25,4 +41,3 @@ RUN conda init bash
 conda run -n ENV_NAME script.sh
 
 
-conda config --add channels bioconda
